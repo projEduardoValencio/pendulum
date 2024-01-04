ad946704f4146e26902625e092826ccc2ba17200
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2024-01-03 23:37:37 -0300

    refactor: alterando projeto para SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,53 @@
-#include "../include/GLFW/glfw3.h"
- 
-#include <stdlib.h>
-#include <stdio.h>
- 
+#include <SDL2/SDL.h>
+#include <iostream>
+using std::cout;
+int main(){
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
 
-static void error_callback(int error, const char* description)
-{
-    fputs(description, stderr);
-}
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-int main(void)
-{
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Event event;
+    int fps = 60;
+    int desiredDelta = 1000/fps;
+
+    bool running = true;
+    SDL_Rect r{10,10, 500/2, 500/2};
+
+    SDL_CreateWindowAndRenderer(500, 500, 0, &window, &renderer);
+    while (running) {
+        int startLoop = SDL_GetTicks();
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT){
+                running = false;
+            }
+            else if(event.type == SDL_KEYDOWN){
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_a:
+                    cout << "Tecla pressionada a\n";
+                    break;
+                default:
+                    break;
+                }
+                cout << "Tecla pressionada: " << event.key.keysym.sym << '\n';
+            }
+            else if(event.type == SDL_MOUSEMOTION){
+                SDL_GetMouseState(&r.x, &r.y);
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderFillRect(renderer, &r);
+
+        SDL_RenderPresent(renderer);
+        
+        int delta = SDL_GetTicks() - startLoop;
+        if(delta < desiredDelta){
+            SDL_Delay(desiredDelta - delta);
+        }
     }
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-    while (!glfwWindowShouldClose(window))
-    {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+    return 0;
 }
